1f594cded6f75f2c0e53cd551098bcfb44d7ed43
Kevin Sony <kevinsony2003@gmail.com>
2019-10-23 20:35:03 -0500

    Added vscode to my laptop

---
+++ Graph_Theory/prob1280.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+map<int, vector<int>> dependencies;
+
+int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+
+    int a, b;
+    for (int i = 0; i < M; i++) {
+        scanf("%d %d", &a, &b);
+        dependencies.
+    }
+
+    int[] plan = new int[N];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &plan[i]);
+    }
+    return 0;
+}
